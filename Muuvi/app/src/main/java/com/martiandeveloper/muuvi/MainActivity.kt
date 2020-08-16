5742ab042bcc97b8ab76de1a9eb3336299050837
mdismayilov <murad.ismaylov.97@gmail.com>
2020-08-16 09:54:56 +0400

    Hello World app is created

---
+++ Muuvi/app/src/main/java/com/martiandeveloper/muuvi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.martiandeveloper.muuvi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
