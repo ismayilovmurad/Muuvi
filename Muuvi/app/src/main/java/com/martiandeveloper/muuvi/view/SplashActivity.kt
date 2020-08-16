683625fe33d856c0d452840ff577f2772a83399a
mdismayilov <murad.ismaylov.97@gmail.com>
2020-08-16 11:58:58 +0400

    Required configurations are added

--- Muuvi/app/src/main/java/com/martiandeveloper/muuvi/view/SplashActivity.kt
+++ Muuvi/app/src/main/java/com/martiandeveloper/muuvi/view/SplashActivity.kt
@@ -1,9 +1,10 @@
-package com.martiandeveloper.muuvi
+package com.martiandeveloper.muuvi.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.martiandeveloper.muuvi.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
